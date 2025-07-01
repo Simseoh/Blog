ebba263a49d7b841617e79055f9ec0658e47afc6
Simseoh <seohoon0707@gmail.com>
2025-07-01 20:41:51 +0900

    fix:inline

--- src/main/kotlin/com/server/webfluxblog/domain/post/controller/PostController.kt
+++ src/main/kotlin/com/server/webfluxblog/domain/post/controller/PostController.kt
@@ -13,8 +13,9 @@ import java.security.Principal
 @RestController
 @RequestMapping("/posts")
 @Tag(name = "Post")
-class PostController(private val postService: PostService) {
-
+class PostController(
+    private val postService: PostService
+) {
     @Operation(summary = "게시물 게시")
     @PostMapping
     suspend fun createPost(@RequestBody request: CreatePostRequest) = postService.createPost(request)
@@ -26,9 +27,11 @@ class PostController(private val postService: PostService) {
     @Operation(summary = "게시물 전체 조회")
     @GetMapping("/recent")
     fun getRecentPosts(): Flow<PostEntity>? = postService.getRecentPosts()
+
     @Operation(summary = "트렌딩게시물 조회")
     @GetMapping("/trending")
     suspend fun getTrendingPosts(): Flow<PostEntity> = postService.getTrendingPosts()
+
     @Operation(summary = "게시물 검색")
     @GetMapping("/search")
     fun searchPosts(@RequestParam query: String): Flow<PostEntity> = postService.searchPosts(query)
