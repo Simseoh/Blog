aa0fafff58fad25b4cbe386232d7a83fd9057cac
Simseoh <seohoon0707@gmail.com>
2025-07-01 10:17:59 +0900

    fix:jwtProperties

--- src/main/kotlin/com/server/webfluxblog/global/security/jwt/property/JwtProperties.kt
+++ src/main/kotlin/com/server/webfluxblog/global/security/jwt/property/JwtProperties.kt
@@ -1,11 +1,13 @@
 package com.server.webfluxblog.global.security.jwt.property
 
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.bind.ConstructorBinding
 import org.springframework.stereotype.Component
 
-@Component
-data class JwtProperties(
-    @Value("\${jwt.secret-key}") val secretKey: String,
-    @Value("\${jwt.access-token-expiration}") val accessTokenExpiration: Long,
-    @Value("\${jwt.refresh-token-expiration}") val refreshTokenExpiration: Long
+@ConfigurationProperties(prefix = "jwt")
+data class JwtProperties @ConstructorBinding constructor (
+    val secretKey: String,
+    val accessTokenExpiration: Long,
+    val refreshTokenExpiration: Long
 )
